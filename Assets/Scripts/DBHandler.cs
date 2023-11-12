3fd943136d26e76b135bd109b5ad5895b42ebf83
Bonny Y. Wang 博尼 / 汪玥 <bonny.y.gardy@gmail.com>
2023-11-12 18:12:22 -0500

    Setup Firebase for Initial Test

---
+++ Assets/Scripts/DBHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+
+public class DBHandler : MonoBehaviour
+{
+    public static DBHandler instance;
+    public DatabaseReference PlayerRef;
+    [SerializeField] UDPPluginScript uDPPluginScript;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }else
+        {
+            Destroy(this);
+        }
+
+        initialization();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void initialization(){
+        PlayerRef = FirebaseDatabase.DefaultInstance.GetReference("Player");
+        StartListener(transform);
+
+    }
+
+    void StartListener(Transform transform) {
+        PlayerRef.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+          if (e2.DatabaseError != null) {
+            Debug.LogError(e2.DatabaseError.Message);
+            return;
+          }
+          if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0) {
+            PlayerInfo playerInfo = PlayerInfo.CreateFromJSON(e2.Snapshot.Child("instance").GetRawJsonValue());
+            Debug.Log(playerInfo.Scared);
+            if(playerInfo.Scared){
+                uDPPluginScript.shake = 4003000050.ToString();
+            }else{
+                uDPPluginScript.shake = 1003000051.ToString();
+            }
+          }
+          
+        };
+    }
+}
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    public bool Scared;
+    public static PlayerInfo CreateFromJSON(string jsonString){
+        return JsonUtility.FromJson<PlayerInfo>(jsonString);
+    }
+}
